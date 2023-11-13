148973ad598fb2d930f0f4cbb5c8cfea9ed5495c
regiofficial0495 <150647805+regiofficial0495@users.noreply.github.com>
2023-11-13 12:46:10 +0700

    Add files via upload

---
+++ dekripsi .py
@@ -0,0 +1,17 @@
+def caesar_decrypt(ciphertext, shift):
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            ascii_offset = ord('A') if char.isupper() else ord('a')
+            decrypted_char = chr((ord(char) - ascii_offset - shift) % 26 + ascii_offset)
+            plaintext += decrypted_char
+        else:
+            plaintext += char
+    return plaintext
+
+ciphertext = "Heru Susanto"
+shift_value = 7
+decrypted_text = caesar_decrypt(ciphertext, shift_value)
+print("Ciphertext:", ciphertext)
+print("Shift Value:", shift_value)
+print("Decrypted Text:", decrypted_text)
